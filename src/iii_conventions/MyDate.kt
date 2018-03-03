4e78299fa48ce7f34530e05443843697851c5c41
wtanaka.com <wtanaka@users.noreply.github.com>
2018-03-03 13:17:23 -1000

    kotlin koans (iii)

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,22 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int = when {
+        this.year != other.year -> this.year - other.year
+        this.month != other.month -> this.month - other.month
+        else -> this.dayOfMonth - other.dayOfMonth
+    }
+}
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+operator fun MyDate.compareTo(other: MyDate): Int = 0
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+operator fun MyDate.plus(interval: TimeInterval) = this.addTimeIntervals(interval, 1)
+
+operator fun MyDate.plus(interval: MultipleTimeIntervals) = this.addTimeIntervals(interval.interval, interval.count)
+
+operator fun TimeInterval.times(count: Int): MultipleTimeIntervals = MultipleTimeIntervals(this, count)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +24,20 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+data class MultipleTimeIntervals(val interval: TimeInterval, val count: Int)
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+            var current: MyDate = start
+            override fun hasNext(): Boolean = current <= endInclusive
+
+            override fun next(): MyDate {
+                val next = current
+                current = current.nextDay()
+                return next
+            }
+        }
+    }
+
+}
--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,17 +3,24 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(val invocationCount: Int = 0) {
+    operator fun invoke(): Invokable {
+        return Invokable(invocationCount = this.invocationCount + 1)
+    }
+
+    fun getNumberOfInvocations(): Int {
+        return invocationCount
+    }
+}
 
 fun todoTask31(): Nothing = TODO(
-    """
+        """
         Task 31.
         Change the class 'Invokable' to count the number of invocations:
         for 'invokable()()()()' it should be 4.
     """,
-    references = { invokable: Invokable -> })
+        references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
